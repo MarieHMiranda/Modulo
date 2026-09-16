b17809e63808856f8ca57910728fb27f6050b1c9
Mariela <marielamiranda.8@gmail.com>
2026-09-16 00:31:36 -0300

    nuevos modulos

---
+++ mi_matematica.py
@@ -0,0 +1,26 @@
+def es_par(numero):
+    par = numero % 2 == 0
+    return par
+
+def el_doble(numero):
+    doble = numero * 2
+    return doble
+
+
+def promedio(lista_numeros):
+    prom = sum(lista_numeros) / len(lista_numeros)
+    return prom
+
+def area_circulo(radio):
+    pi = 3.14159
+    area = pi * radio ** 2
+    return area
+
+def mayor_que(numero1, numero2):
+    ok = numero1 > numero2
+    return ok
+
+def suma(num1,num2,num3):
+    suma = num1 + num2 + num3
+    return suma
+    
---
+++ principal.py
@@ -0,0 +1,8 @@
+import mi_matematica
+
+print(mi_matematica.es_par(9))
+print(mi_matematica.promedio([4, 8, 15, 16, 23, 42]))
+print(mi_matematica.el_doble(5))
+print(mi_matematica.area_circulo(3))
+print(mi_matematica.mayor_que(30, 20))
+print(mi_matematica.suma(15, 10, 15))
